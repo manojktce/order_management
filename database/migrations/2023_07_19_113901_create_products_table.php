4a4e297197e25eac9c98dddfd784f229e02309ea
webwizardsusa <webwizardsusa@gmail.com>
2023-07-19 17:23:10 +0530

    Products table migration file added

---
+++ database/migrations/2023_07_19_113901_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->nullable(false);
+            $table->integer('category_id')->nullable(false);
+            $table->string('title')->nullable(false)->unique();
+            $table->text('description')->nullable(false); 
+            $table->decimal('price',9,2); 
+            $table->integer('qty')->default(5); // Set Minimum 5 default quantity for new product
+            $table->enum('status', [0, 1])->default(1); // 0-Inactive , 1-Active
+            $table->datetime('created_at')->useCurrent();
+            $table->datetime('updated_at')->nullable()->userCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
