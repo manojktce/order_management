4ee05a6a85e7b47476358b901180749b0e975f34
webwizardsusa <webwizardsusa@gmail.com>
2023-07-19 17:04:35 +0530

    User Table Migration Modified

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->text('stripe_id')->nullable();
             $table->enum('status', [0, 1])->default(1); // 0-Inactive , 1-Active
             $table->datetime('created_at')->useCurrent();
-            $table->datetime('updated_at')->userCurrent()->useCurrentOnUpdate();
+            $table->datetime('updated_at')->nullable()->userCurrent()->useCurrentOnUpdate();
         });
     }
 
