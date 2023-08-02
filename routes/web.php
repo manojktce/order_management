6f57b90ba5c3f915fdac88bec238fa2b7cf2e8a2
webwizardsusa <webwizardsusa@gmail.com>
2023-08-02 11:51:41 +0530

    Route Changes

--- routes/web.php
+++ routes/web.php
@@ -25,19 +25,15 @@ Route::post('admin_login', [App\Http\Controllers\SiteAdminController::class, 'ad
 
 Auth::routes();
 
+/* Vendor Routes Start */
 Route::group(['middleware' => ['role:Vendor']], function () {
     Route::resource('vendor_product',VendorProductController::class);
 });
+/* Vendor Routes End */
 
+
+/* User Routes Start */
 Route::group(['middleware' => ['role:User']], function () {
-    /* Cart Controller Start*/
-    Route::get('showCart', [CartController::class, 'showCart'])->name('showCart');
-    Route::get('addToCart/{id}', [CartController::class, 'addToCart'])->name('addToCart');
-    Route::get('updateCart/{row_id}/{option}', [CartController::class, 'updateCart'])->name('updateCart');
-    Route::get('deleteCart/{id}', [CartController::class, 'deleteCart'])->name('deleteCart');
-    Route::get('clearCart', [CartController::class, 'clearCart'])->name('clearCart');
-    /* Cart Controller End*/
-
     /* Payment Controller Start */
     Route::post('purchase',[PaymentController::class, 'purchase_items'])->name('purchase');
     /* Payment Controller End */
@@ -47,8 +43,18 @@ Route::group(['middleware' => ['role:User']], function () {
     Route::get('order_details/{id}',[UserOrdersController::class, 'order_details'])->name('order_details');
     /* Orders Controller End */
 });
+/* User Routes End */
+
+/* Cart Routes Start*/
+Route::get('showCart', [CartController::class, 'showCart'])->name('showCart');
+Route::get('addToCart/{id}', [CartController::class, 'addToCart'])->name('addToCart');
+Route::get('updateCart/{row_id}/{option}', [CartController::class, 'updateCart'])->name('updateCart');
+Route::get('deleteCart/{id}', [CartController::class, 'deleteCart'])->name('deleteCart');
+Route::get('clearCart', [CartController::class, 'clearCart'])->name('clearCart');
+/* Cart Routesr End*/
 
 
+/* Admin Routes Start */
 Route::group(['middleware' => ['role:Admin']], function () {
     Route::get('/adminHome', [App\Http\Controllers\SiteAdminController::class, 'adminHome'])->name('adminHome');
     
@@ -61,3 +67,4 @@ Route::group(['middleware' => ['role:Admin']], function () {
 
     Route::post('admin_logout', [App\Http\Controllers\SiteAdminController::class, 'admin_logout'])->name('admin_logout');
 });
+/* Admin Routes End */
