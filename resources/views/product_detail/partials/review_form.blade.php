5599fd3178403cea05de49f5879bfb215987084a
webwizardsusa <webwizardsusa@gmail.com>
2023-08-04 13:23:36 +0530

    Review Section

--- resources/views/product_detail/partials/review_form.blade.php
+++ resources/views/product_detail/partials/review_form.blade.php
@@ -1,18 +1,3 @@
-<div class="review_box">
-  <p>Your Rating: 
-  @foreach($result['ratings'] as $res)
-    @php
-      if($res->users_id != Auth::User()->id)
-      continue;
-    @endphp
-
-    @for($i=1; $i<=$res->rating; $i++)
-      <i class="fa fa-star"></i>
-    @endfor
-  @endforeach
-  </p> 
-</div>
-
 <div class="review_box mt-4">
   <h4>Add a Review</h4>
   <form class="row contact_form" action="{{ route('add_review', encrypt($result['products']->id)) }}" method="post" novalidate="novalidate">
